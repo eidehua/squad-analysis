a5d031a3c259977500e30b7ff6a88c697816aae8
Edward Xue <edthemaster1@gmail.com>
2018-04-30 22:24:18 -0500

    added initial analysis files

---
+++ analyzer.py
@@ -0,0 +1,4 @@
+import json
+
+with open('data/sorted-test.json') as data_file:
+	data = json.load(data_file)
---
+++ sampler.py
@@ -0,0 +1,15 @@
+
+out_jsondata = {}
+out_jsondata['data'] = []
+
+for i in range(0, len(out_jsondata['data']):
+	l = len(jsondata['data'][i]['paragraphs'])
+	a = random(0,40)
+	b = random(0,40)
+	(if a == b)
+	  b = a + 1
+	curr_paras = [jsondata['data'][i]['paragraphs'][a], jsondata['data'][i]['paragraphs'][b]] 
+	out_object = {}
+	out_object['paragraphs'] = curr_pars
+	out_object['title'] = jsondata['data'][i]['title']
+	out_jsondata['data'].append(out_object)
