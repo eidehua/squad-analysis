47fe86a5e3881bbd5552295f1b647728603ba62b
eidehua <edthemaster1@gmail.com>
2018-05-01 11:55:15 -0500

    making progressing with analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,23 @@
 import json
+from collections import Counter
+categories = Counter()
+reasoning = Counter()
 
-with open('data/sorted-test.json') as data_file:
+with open('data/sorted-dev.json') as data_file:
 	data = json.load(data_file)
+	it = iter(sorted(data.items()))
+	#print(next(it))
+	with open('data/labled-dev.json') as labeled_file:
+		labeled_data = json.load(labled_file)
+		#labled_data['data'][article_index]['paragraphs'][paragraph_index]['qas']
+		for article in labeled_data['data']:
+			for paragraph in article['paragraph']:
+				for qa in paragraph['qas']:
+					if "category" in qa:
+						# iterated through our sorted dev json file (which is a superset of labeled dev)
+						data_tuple = next(it)
+						while (data_tuple[0] != qa['id']):
+							data_tuple = next(it)
+						
+		#for id in data:
+			
--- sorter.py
+++ sorter.py
@@ -1,6 +1,6 @@
 import json
 
-with open('data/test-009000.json') as data_file:
+with open('data/ensemble.json') as data_file:
 	data = json.load(data_file)
-	with open('data/sorted-test.json', 'w') as out_file:
+	with open('data/sorted-ensemble.json', 'w') as out_file:
 		json.dump(data, out_file, sort_keys=True)
